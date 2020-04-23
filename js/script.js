9de6d6c52aff578c4786c1a6087c17ec9df78af3
Alex <alex.dev090283@gmail.com>
2020-04-23 16:44:14 +0300

    ES6 add div

--- js/script.js
+++ js/script.js
@@ -111,4 +111,14 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+    class Options {
+        constructor (height, width, bg, fontSize, textAlign) {
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+    }
+
 });
